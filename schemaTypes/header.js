219f5eac517a5669366abfaaabff52539fe0b125
DevNakatori <dev@nakatori.agency>
2024-10-07 17:28:49 +0530

    menu from sanity

---
+++ schemaTypes/header.js
@@ -0,0 +1,58 @@
+import {defineField, defineType, isImage} from 'sanity'
+
+export default defineType({
+  name: 'header',
+  title: 'Header',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'header',
+      title: 'Header',
+      type: 'object',
+      fields: [
+        defineField({
+          name: 'menu',
+          title: 'Menu',
+          type: 'array',
+          of: [
+            {
+              type: 'object',
+              fields: [
+                defineField({
+                  name: 'label',
+                  title: 'Label',
+                  type: 'string',
+                }),
+                defineField({
+                    name: 'link',
+                    title: 'Link',
+                    type: 'url',
+                }),
+              ],
+            },
+          ],
+        }),
+      ],
+    }),
+
+    defineField({
+      name: 'language',
+      type: 'string',
+      readOnly: true,
+    }),
+  ],
+  preview: {
+    select: {
+      language: 'language',
+    },
+    prepare(selection) {
+      const { topTitle, bottomTitle, language, media } = selection;
+
+      return {
+        title: `${topTitle || ''} ${bottomTitle || ''}`.trim(),
+        subtitle: language,
+        media,
+      }
+    },
+  },
+})
